c05f5c324f0d9efb6638df1a4f31c18622503032
Jakob Heuser <jakob@codedrift.com>
2025-07-01 18:17:48 -0700

    chore: Updates pack to 0.0.4

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,10 +1,11 @@
+import pk from "../package.json" with { type: "json" };
 import { type Manifest } from "./__generated__/manifest.js";
 
 export const manifest = {
   schema: "pre2",
   name: "testpack",
-  version: "0.0.1",
-  description: "Test pack, never published to prod",
+  version: pk.version,
+  description: "Test / example pack, never published to prod",
   permissions: {
     body: true, // wants access to the body of the request and response
   },
